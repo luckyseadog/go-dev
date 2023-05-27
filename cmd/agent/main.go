a738d4c95e2199dea20cfd474fa4517c9728ed9f
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-05-27 13:09:43 +0300

    Made first version of agent in internal/agent/agent.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import (
+	"github.com/luckyseadog/go-dev/internal/agent"
+	"time"
+)
+
+func main() {
+	agent := agent.NewAgent(":8080", "text/plain", 2*time.Second, 10*time.Second)
+	agent.Run()
+	time.Sleep(20 * time.Second)
+}
