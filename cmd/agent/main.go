55d78790af40ef7bc9d4ccdd7b4c2e6bfd3a61ca
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-05-27 20:53:14 +0300

    del main

--- cmd/agent/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "time"
-
-func main() {
-	agent := NewAgent("http://127.0.0.1:8080", "text/plain", 2*time.Second, 10*time.Second)
-	agent.Run()
-	time.Sleep(5 * time.Minute)
-}
