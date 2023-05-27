3d311d428ea0cf3b0ca743d8fdbf0bd7499dc648
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-05-27 22:39:36 +0300

    back to main and agent

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mainHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, req.URL.String())
+		fmt.Println(req.URL.String())
+	}
+
+	http.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe(":8083", nil))
+}
