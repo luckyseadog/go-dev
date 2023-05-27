3b3dc7b5bb40df6ffb5550f90db5358766da2727
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-05-27 23:23:02 +0300

    30 sec

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,5 +5,5 @@ import "time"
 func main() {
 	agent := NewAgent("http://127.0.0.1:8080", "text/plain", 2*time.Second, 10*time.Second)
 	agent.Run()
-	time.Sleep(5 * time.Minute)
+	time.Sleep(30 * time.Second)
 }
