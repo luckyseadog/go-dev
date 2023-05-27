d4d561d3dad88dcf5409c30d16a052e7f300f236
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-05-27 20:50:59 +0300

    main in agent

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -138,9 +138,14 @@ func (a *Agent) PostStats() {
 func (a *Agent) Run() {
 	go a.GetStats()
 	go a.PostStats()
-	<-a.cancel
 }
 
 func (a *Agent) Stop() {
 	close(a.cancel)
+}
+
+func main() {
+	agent := NewAgent("http://127.0.0.1:8080", "text/plain", 2*time.Second, 10*time.Second)
+	agent.Run()
+	time.Sleep(5 * time.Minute)
 }
