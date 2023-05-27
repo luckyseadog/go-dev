dc7904dfeee2e7cb606ca35065e555fcd72d0d9c
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-05-27 19:28:32 +0300

    corrected the error in the title

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	agent := NewAgent(":8080", "text/plain", 2*time.Second, 10*time.Second)
+	agent := NewAgent("http://localhost:8080", "text/plain", 2*time.Second, 10*time.Second)
 	agent.Run()
 	time.Sleep(5 * time.Minute)
 }
