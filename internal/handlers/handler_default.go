0457c5debed0dbdf591e3f0bef9e4db47e2bf0ff
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-09-02 17:43:57 +0300

    add more readable errors discriptions

--- internal/handlers/handler_default.go
+++ internal/handlers/handler_default.go
@@ -27,39 +27,39 @@ func HandlerDefault(w http.ResponseWriter, r *http.Request, storage storage.Stor
 	w.WriteHeader(http.StatusOK)
 	_, err := fmt.Fprintf(w, "<html><body>")
 	if err != nil {
-		http.Error(w, "HandlerDefault: error when writing to html", http.StatusInternalServerError)
+		http.Error(w, "HandlerDefault: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	res := storage.LoadDataGaugeContext(r.Context())
 	if res.Err != nil {
-		http.Error(w, "HandlerDefault: error when writing to html", http.StatusInternalServerError)
+		http.Error(w, "HandlerDefault: "+res.Err.Error(), http.StatusInternalServerError)
 		return
 	}
 	for key := range res.Value.(map[metrics.Metric]metrics.Gauge) {
 		_, err = fmt.Fprintf(w, "<p>%s</p>", string(key))
 		if err != nil {
-			http.Error(w, "HandlerDefault: error when writing to html", http.StatusInternalServerError)
+			http.Error(w, "HandlerDefault: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 
 	res = storage.LoadDataCounterContext(r.Context())
 	if res.Err != nil {
-		http.Error(w, "HandlerDefault: error when writing to html", http.StatusInternalServerError)
+		http.Error(w, "HandlerDefault: "+res.Err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	for key := range res.Value.(map[metrics.Metric]metrics.Counter) {
 		_, err = fmt.Fprintf(w, "<p>%s</p>", string(key))
 		if err != nil {
-			http.Error(w, "HandlerDefault: error when writing to html", http.StatusInternalServerError)
+			http.Error(w, "HandlerDefault: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 	_, err = fmt.Fprintf(w, "</body></html>")
 	if err != nil {
-		http.Error(w, "HandlerDefault: error when writing to html", http.StatusInternalServerError)
+		http.Error(w, "HandlerDefault: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
